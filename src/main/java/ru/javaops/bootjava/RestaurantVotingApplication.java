054cb5e6da2034fd149b4e59f01e2a51fbac6bd8
ValeryManakov <valera.manakov.93@bk.ru>
2021-12-30 08:36:39 +0300

    2_02_h2_init

--- src/main/java/ru/javaops/bootjava/RestaurantVotingApplication.java
+++ src/main/java/ru/javaops/bootjava/RestaurantVotingApplication.java
@@ -5,12 +5,7 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import ru.javaops.bootjava.model.Role;
-import ru.javaops.bootjava.model.User;
 import ru.javaops.bootjava.repository.UserRepository;
-
-import java.util.Arrays;
-import java.util.HashSet;
 
 @SpringBootApplication
 @AllArgsConstructor
@@ -23,8 +18,6 @@ public class RestaurantVotingApplication implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) {
-        userRepository.save(new User("user@gmail.com", "User_First", "User_Last", "password", new HashSet(Arrays.asList(Role.ROLE_USER))));
-        userRepository.save(new User("admin@javaops.ru", "Admin_First", "Admin_Last", "admin", new HashSet(Arrays.asList(Role.ROLE_USER, Role.ROLE_ADMIN))));
         System.out.println(userRepository.findAll());
     }
 }
